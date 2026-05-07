86b90f0c706a8f266f4d7991d96cfeeef5c5ccb6
Yuyi-Hao <roshanisingh2354@gmail.com>
2026-05-07 21:52:27 +0530

    [FIX] fix wrong approach for problem

--- assignment-01/PR-03/solver.c
+++ assignment-01/PR-03/solver.c
@@ -19,21 +19,59 @@ int q_pop(int *queue, int *left, int *size, int capacity){
     return val;
 }
 
+int q_peek(int *queue, int left, int size){
+    if(size==0) return -1;
+    return queue[left];
+}
+
+int q_peek_back(int *queue, int right, int size, int capacity){
+    if(size==0) return -1;
+    int idx  = (right-1)%capacity;
+    return queue[idx];
+}
 int *network_packet_sim(int buffer_size, int packet_count, int **packets, int *result_size) {
-    int *res = malloc(sizeof(int)*packet_count);
-    for(int i=0; i<packet_count; i++) res[i] = -1;
-    int *queue = (int *)malloc(sizeof(int)*buffer_size);
-    int queue_size = 0, q_left_idx=0, q_right_idx=0;
-    int curr_idx = 0;
-    int curr_time = 0;
-    for(; curr_idx < packet_count; curr_idx++){
-        if(curr_time <= packets[curr_idx][0]) q_push(queue, &q_right_idx, &queue_size, buffer_size, curr_idx);
-        int pop_idx = q_pop(queue, &q_left_idx, &queue_size, buffer_size);
-        if(pop_idx == -1) continue;
-        res[pop_idx] = fmax(curr_time, packets[pop_idx][0]);
-        curr_time += packets[pop_idx][1];
+    int *res = malloc(sizeof(int) * packet_count);
+    for (int i = 0; i < packet_count; i++)
+        res[i] = -1;
+
+    // Queue holds finish times
+    int *queue = malloc(sizeof(int) * buffer_size);
+    int left = 0, right = 0, size = 0;
+
+    for (int i = 0; i < packet_count; i++) {
+        int arrival = packets[i][0];
+        int duration = packets[i][1];
+
+        // 1. Remove all finished packets
+        while (size > 0 && queue[left] <= arrival) {
+            left = (left + 1) % buffer_size;
+            size--;
+        }
+
+        // 2. Drop if full
+        if (size == buffer_size) {
+            res[i] = -1;
+            continue;
+        }
+
+        // 3. Compute start time
+        int start = arrival;
+        if (size > 0) {
+            int last_idx = (right - 1 + buffer_size) % buffer_size;
+            int last_finish = queue[last_idx];
+            if (last_finish > arrival)
+                start = last_finish;
+        }
+
+        res[i] = start;
+
+        // 4. Push finish time
+        queue[right] = start + duration;
+        right = (right + 1) % buffer_size;
+        size++;
     }
-   
+
+    free(queue);
     *result_size = packet_count;
     return res;
 }
